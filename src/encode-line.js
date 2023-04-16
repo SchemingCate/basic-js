0eca15c06fbda90b108392b4ee30c4cf8bf56f5d
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-04-16 23:40:12 +0300

    implement encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+let strArr = str.split('');
+let outputArr = []
+let dublicatesCount = 1;
+for (let i = 0; i < strArr.length; i++) {
+  if (strArr[i] === strArr[i + 1]) {
+    dublicatesCount += 1;
+    continue;
+  }
+  if (strArr[i - 1] === strArr[i]) {
+    outputArr.push(`${dublicatesCount}${strArr[i]}`);
+    dublicatesCount = 1;
+    continue;
+  }
+  outputArr.push(strArr[i]);
+}
+return outputArr.join('');
 }
 
 module.exports = {
